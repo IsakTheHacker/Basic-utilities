c88aa62c62f64438470cdafc2b2c71d854797776
IsakTheHacker <isak.brynielsson@gmail.com>
2021-01-10 21:06:14 +0100

    Try to implement a first version of countdown.

--- Countdown/src/main.cpp
+++ Countdown/src/main.cpp
@@ -2,28 +2,65 @@
 #pragma warning(disable : 4996) //_CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <array>
 #include <ctime>
-
+#include <chrono>
 
 std::array<std::string, 7> days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+std::array<std::string, 12> months = { "January", "February", "Marsch", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
 
 
 int main() {
+
+	//Now
 	std::time_t current_time = time(0);
-	std::tm* tm_local = std::localtime(&current_time);
+	std::tm* now = std::localtime(&current_time);
+	now->tm_year += 1900;
+	now->tm_mon += 1;
 
-	std::string convertedDateTime =
-		std::to_string(tm_local->tm_year + 1900) +
-		"-" + std::to_string(tm_local->tm_mon + 1) +
-		"-" + std::to_string(tm_local->tm_mday) +
-		" " + std::to_string(tm_local->tm_hour) +
-		"." + std::to_string(tm_local->tm_min) +
-		"." + std::to_string(tm_local->tm_sec)
-	;
+	//Birthday
+	std::tm* birthday = std::localtime(&current_time);
+	birthday->tm_year += 1900;
+	birthday->tm_mon += 1;
 
-	std::cout << days[tm_local->tm_wday] << std::endl;
+	int year = birthday->tm_year;
+	int month = birthday->tm_mon;
+	int date = birthday->tm_mday;
+	int hour = birthday->tm_hour;
+	int min = birthday->tm_min;
+	int sec = birthday->tm_sec;
+
+	//Enter year
+	std::cout << "Enter year (" << birthday->tm_year << "): ";
+	std::cin >> birthday->tm_year;
+	birthday->tm_year -= 1900;
+
+	//Enter month
+	std::cout << "Enter month (" << birthday->tm_mon << "): ";
+	std::cin >> birthday->tm_mon;
+	birthday->tm_year -= 1;
+
+	//Enter date
+	std::cout << "Enter date (" << birthday->tm_mday << "): ";
+	std::cin >> birthday->tm_mday;
+
+	//Enter hour
+	std::cout << "Enter hour (" << birthday->tm_hour << "): ";
+	std::cin >> birthday->tm_hour;
+
+	//Enter minute
+	std::cout << "Enter minute (" << birthday->tm_min << "): ";
+	std::cin >> birthday->tm_min;
+
+	//Enter second
+	std::cout << "Enter second (" << birthday->tm_sec << "): ";
+	std::cin >> birthday->tm_sec;
+
+	std::cout << std::mktime(now) << std::endl;
+	std::cout << std::mktime(birthday) << std::endl;
+	std::cout << std::difftime(std::mktime(now), std::mktime(birthday)) << std::endl;
 
 	return 0;
 }
