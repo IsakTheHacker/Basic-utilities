7b7509fbf8d7ccb47d913c08d3c2411a907416d6
IsakTheHacker <isak.brynielsson@gmail.com>
2021-01-10 21:59:57 +0100

    Do some more testing.

--- Countdown/src/main.cpp
+++ Countdown/src/main.cpp
@@ -6,61 +6,72 @@
 #include <string>
 #include <array>
 #include <ctime>
-#include <chrono>
+#include <thread>
 
 std::array<std::string, 7> days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 std::array<std::string, 12> months = { "January", "February", "Marsch", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
 
 
 int main() {
-
-	//Now
-	std::time_t current_time = time(0);
-	std::tm* now = std::localtime(&current_time);
-	now->tm_year += 1900;
-	now->tm_mon += 1;
-
-	//Birthday
-	std::tm* birthday = std::localtime(&current_time);
-	birthday->tm_year += 1900;
-	birthday->tm_mon += 1;
-
-	int year = birthday->tm_year;
-	int month = birthday->tm_mon;
-	int date = birthday->tm_mday;
-	int hour = birthday->tm_hour;
-	int min = birthday->tm_min;
-	int sec = birthday->tm_sec;
+	
+	std::time_t birthday = time(0);
+	std::tm* localTime = std::localtime(&birthday);
 
 	//Enter year
-	std::cout << "Enter year (" << birthday->tm_year << "): ";
-	std::cin >> birthday->tm_year;
-	birthday->tm_year -= 1900;
+	std::cout << "Enter year (" << localTime->tm_year+1900 << "): ";
+	std::cin >> localTime->tm_year;
 
 	//Enter month
-	std::cout << "Enter month (" << birthday->tm_mon << "): ";
-	std::cin >> birthday->tm_mon;
-	birthday->tm_year -= 1;
+	std::cout << "Enter month (" << localTime->tm_mon+1 << "): ";
+	std::cin >> localTime->tm_mon;
 
 	//Enter date
-	std::cout << "Enter date (" << birthday->tm_mday << "): ";
-	std::cin >> birthday->tm_mday;
+	std::cout << "Enter date (" << localTime->tm_mday << "): ";
+	std::cin >> localTime->tm_mday;
 
 	//Enter hour
-	std::cout << "Enter hour (" << birthday->tm_hour << "): ";
-	std::cin >> birthday->tm_hour;
+	std::cout << "Enter hour (" << localTime->tm_hour << "): ";
+	std::cin >> localTime->tm_hour;
 
 	//Enter minute
-	std::cout << "Enter minute (" << birthday->tm_min << "): ";
-	std::cin >> birthday->tm_min;
+	std::cout << "Enter minute (" << localTime->tm_min << "): ";
+	std::cin >> localTime->tm_min;
 
 	//Enter second
-	std::cout << "Enter second (" << birthday->tm_sec << "): ";
-	std::cin >> birthday->tm_sec;
+	std::cout << "Enter second (" << localTime->tm_sec << "): ";
+	std::cin >> localTime->tm_sec;
 
-	std::cout << std::mktime(now) << std::endl;
-	std::cout << std::mktime(birthday) << std::endl;
-	std::cout << std::difftime(std::mktime(now), std::mktime(birthday)) << std::endl;
+	localTime->tm_year -= 1900;
+	localTime->tm_mon -= 1;
+	birthday = std::mktime(localTime);
+
+
+
+
+	while (true) {
+		std::time_t now = time(0);
+		time_t diff = std::difftime(birthday, now);
+		//std::cout << std::ctime(&diff) << std::endl;
+
+		
+		using namespace std::chrono_literals;
+		std::this_thread::sleep_for(1s);
+
+		localTime = std::localtime(&diff);
+		localTime->tm_year -= 70;
+
+		char buffer[30];
+		std::strftime(buffer, sizeof(buffer), "%y", localTime);
+		std::cout << buffer << std::endl;
+
+		/*std::cout << std::asctime(localTime);
+		std::cout << "Years: " << localTime->tm_year << std::endl;
+		std::cout << "Months: " << localTime->tm_mon << std::endl;
+		std::cout << "Days: " << localTime->tm_mday << std::endl;
+		std::cout << "Hours: " << localTime->tm_hour << std::endl;
+		std::cout << "Minutes: " << localTime->tm_min << std::endl;
+		std::cout << "Seconds: " << localTime->tm_sec << std::endl;*/
+	}
 
 	return 0;
 }
