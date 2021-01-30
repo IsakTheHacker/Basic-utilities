93f430564ef5552fc656c80287719ab8eb6362cc
IsakTheHacker <isak.brynielsson@gmail.com>
2021-01-30 02:58:51 +0100

    Add basic exception handling.

--- Mouse Utility/Mouse_Utility.py
+++ Mouse Utility/Mouse_Utility.py
@@ -3,4 +3,8 @@
 import mouse
 
 while True:
-	print("\rX: " + str(mouse.get_position()[0]) + ", Y: " + str(mouse.get_position()[1]), end="")
+	try:
+		print("\rX: " + str(mouse.get_position()[0]) + ", Y: " + str(mouse.get_position()[1]), end="")
+	except KeyboardInterrupt:
+		print("\nBye!\n")
+		break
