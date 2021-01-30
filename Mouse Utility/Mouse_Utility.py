b544c3ad4249bdf47c7b0987d870f0b56437988f
IsakTheHacker <isak.brynielsson@gmail.com>
2021-01-30 03:17:28 +0100

    Test making an auto clicker.

--- Mouse Utility/Mouse_Utility.py
+++ Mouse Utility/Mouse_Utility.py
@@ -1,10 +1,15 @@
 
 #Imports
 import mouse
+import keyboard
 
 while True:
 	try:
 		print("\rX: " + str(mouse.get_position()[0]) + ", Y: " + str(mouse.get_position()[1]), end="")
 	except KeyboardInterrupt:
 		print("\nBye!\n")
+		break
+
+	mouse.click()
+	if (keyboard.is_pressed("alt")):
 		break
